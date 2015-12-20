be844fd17fe636f02f0a55f739f58c2d01272126
xuhuan <xuhuanxxx@gmail.com>
2015-12-20 18:02:20 +0800

    1

--- OperateExcel/ExcelReader.cs
+++ OperateExcel/ExcelReader.cs
@@ -2,102 +2,97 @@
 using System.IO;
 using System.Data;
 using System.Collections.Generic;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
+using System.Linq;
+using NPOI;
+using NPOI.XSSF;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
 
 namespace Excel
 {
     public class ExcelReader
     {
-        private static void Initialize(string fileName, out Excel.Application xlApp, out Excel.Workbook xlWorkBook)
+        private static void Initialize(string filePath, out IWorkbook workbook)
         {
-
+            if (!File.Exists(filePath))
+            {
+                //return null;
+            }
+            if (! filePath.EndsWith(".xls")| !filePath.EndsWith(".xlsx") )
+            {
+                //return null;
+            }
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                workbook = WorkbookFactory.Create(fs);
+            }
         }
 
 
-
-
-        public static DataSet ImportDataFromAllSheets(string fileName)
+        public static DataSet getAllSheets(string filePath)
         {
-            if (!File.Exists(fileName))
-            {
-                return null;
-            }
-
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Initialize(fileName, out xlApp, out xlWorkBook);
-
-            int workSheetNum = xlWorkBook.Worksheets.Count;
-            int sheetCount=0;
-            DataSet sheets = new DataSet();
-            //Console.WriteLine("Sheet number: " + workSheetNum);
+            IWorkbook workbook;
+            Initialize(filePath, out workbook);
+            int sheetsCount = workbook.NumberOfSheets;
+            DataSet sheetsSet = new DataSet();
+            Console.WriteLine("Sheet number: " + sheetsCount);
             try
             {
-                for (sheetCount = 1; sheetCount <= workSheetNum; sheetCount++)
+                for (int i  = 0; i < sheetsCount; i++)
                 {
-                    //Console.WriteLine("Reading sheet{0}: {1}", sheetCount, xlWorkBook.Sheets[sheetCount].Name);
-                    DataTable sheetData = ExtractDataFromSingleSheet(xlWorkBook, sheetCount);
-                    sheets.Tables.Add(sheetData);
+                    DataTable sheetData = getOneSheet(workbook, i);
+                    sheetsSet.Tables.Add(sheetData);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-            finally
-            {
-                Dispose(fileName, ref xlApp, ref xlWorkBook);
-            }
-
-            return sheets;
+            return sheetsSet;
         }
 
-        public static DataTable ImprotDataFromSingleSheet(string fileName, int sheetIndex)
+        public static DataTable getOneSheet(string filePath, int sheetIndex)
         {
-            if(!File.Exists(fileName))
+            if(!File.Exists(filePath))
             {
                 return null;
             }
 
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Initialize(fileName, out xlApp, out xlWorkBook);
+            IWorkbook workbook;
+            Initialize(filePath, out workbook);
 
-            DataTable sheetData = null;
+            DataTable sheetData = new DataTable();
 
             try
             {
-                sheetData = ExtractDataFromSingleSheet(xlWorkBook, sheetIndex);
+                getOneSheet(workbook, sheetIndex);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-            finally
-            {
-                Dispose(fileName, ref xlApp, ref xlWorkBook);
-            }
 
             return sheetData;
         }
 
-        private static DataTable ExtractDataFromSingleSheet(Excel.Workbook xlWorkBook, int sheetCount)
+        private static DataTable getOneSheet(IWorkbook workbook, int sheetIndex)
         {
-            int rowCount = 0;
-            int columnCount = 0;
             DataTable sheetData = new DataTable();
-
-            Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(sheetCount);
-            Excel.Range range = xlWorkSheet.UsedRange;
-            sheetData.TableName = xlWorkSheet.Name;
-
-            for (int i = 0; i < range.Columns.Count; i++)
+            ISheet sheet = workbook.GetSheetAt(sheetIndex);
+            int rowCount = sheet.LastRowNum;
+            int columnCount = sheet.GetRow(0).LastCellNum;
+            for (int i = 0; i < rowCount; i++)
             {
-                //sheetData.Columns.Add(new DataColumn()); 
-                object cell = (range.Cells[1, i+1] as Excel.Range).Value2;
-                string columnName = cell != null ? (range.Cells[1, i+1] as Excel.Range).Value2.ToString() : String.Empty;
+                if(columnCount < sheet.GetRow(i).LastCellNum)
+                {
+                    columnCount = sheet.GetRow(i).LastCellNum;
+                }
+            }
+
+            for (int i = 0; i < columnCount; i ++)
+            {
+                object cell = sheet.GetRow(0).GetCell(i);
+                string columnName = cell != null ? cell.ToString() : string.Empty;
                 DataColumn column = new DataColumn();
                 column.ColumnName = columnName;
                 column.DataType = Type.GetType("System.Object");
@@ -106,12 +101,13 @@ namespace Excel
 
             try
             {
-                for (rowCount = 1; rowCount <= range.Rows.Count; rowCount++)
+                for (int i = 0; i < rowCount; rowCount ++)
                 {
                     DataRow row = sheetData.NewRow();
-                    for (columnCount = 1; columnCount <= range.Columns.Count; columnCount++)
+                    for (int j = 0; j < columnCount; j++)
                     {
-                        object cellText = (range.Cells[rowCount, columnCount] as Excel.Range).Value;
+                        object cellText = sheet.GetRow(i).GetCell(j);
+                        Console.WriteLine(cellText);
                         double textColor = 0;
                         string textFormat = "G/通用格式";
                         double bgColor = 0;
@@ -121,13 +117,15 @@ namespace Excel
                         }
                         else
                         {
-                            cellText = (range.Cells[rowCount, columnCount] as Excel.Range).Value.ToString();
-                            textColor = (range.Cells[rowCount, columnCount] as Excel.Range).Font.Color;
-                            textFormat = (range.Cells[rowCount, columnCount] as Excel.Range).NumberFormatLocal;
-                            bgColor = (range.Cells[rowCount, columnCount] as Excel.Range).Interior.Color;
+                            cellText = sheet.GetRow(i).GetCell(j).ToString();
+                            /*
+                            textColor = sheet.GetRow(rowIndex).GetCell(columnIndex).CellStyle.GetFont();
+                            textFormat = 
+                            bgColor = 
+                            */
                         }
                         Dictionary<string, object> cell = new Dictionary<string, object> { { "text", cellText }, { "color", textColor}, { "format", textFormat }, { "bgColor", bgColor} };
-                        row[columnCount - 1] = cell;
+                        row[j] = cellText;
                     }
                     sheetData.Rows.Add(row);
                 }
@@ -136,40 +134,8 @@ namespace Excel
             {
                 Console.WriteLine(e.ToString());
             }
-            finally
-            {
-                ReleaseObject(xlWorkSheet);
-            }
-
             return sheetData;
-        }
-
-        private static void Dispose(string fileName, ref Excel.Application xlApp, ref Excel.Workbook xlWorkBook)
-        {
-            xlWorkBook.Close(false, fileName, null);
-            xlApp.Quit();
-
-            ReleaseObject(xlWorkBook);
-            ReleaseObject(xlApp);
-        }
-
-        private static void ReleaseObject(object obj)
-        {
-            try
-            {
-                Marshal.ReleaseComObject(obj);
-                obj = null;
-            }
-            catch (Exception ex)
-            {
-                obj = null;
-                Console.WriteLine(ex.ToString());
-            }
-            finally
-            {
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-            }
+            
         }
     }
 }
--- OperateExcel/Program.cs
+++ OperateExcel/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel;
+using System.Data;
 
 namespace OperateExcel
 {
@@ -10,6 +12,22 @@ namespace OperateExcel
     {
         static void Main(string[] args)
         {
+            string file = @"test.xlsx";
+            DataSet sheets;
+            sheets = ExcelReader.getAllSheets(file);
+            for(int i = 0; i < sheets.Tables.Count; i ++)
+            {
+                DataTable dt = sheets.Tables[i];
+                for(int j = 0; j < dt.Rows.Count; j++)
+                {
+                    for(int k = 0; k < dt.Columns.Count; k++)
+                    {
+                        Console.Write(dt.Rows[i][k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
