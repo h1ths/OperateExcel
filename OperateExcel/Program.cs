dba886135132b2152ac7083c3091f67019721204
xuhuan <xuhuanxxx@gmail.com>
2015-12-22 01:05:31 +0800

    fixbug

--- OperateExcel/Program.cs
+++ OperateExcel/Program.cs
@@ -2,16 +2,64 @@
 using System.Data;
 using System.IO;
 using Excel;
+using System.Collections.Generic;
 
 namespace OperateExcel
 {
     class Program
     {
+
+        public static void regulateData(DataTable dt, int width)
+        {
+            if (dt.Columns.Count > width)
+            {
+                for (int i = width; i < dt.Columns.Count; i++)
+                {
+                    dt.Columns.RemoveAt(i);
+                }
+            }
+            else
+            {
+                width = dt.Columns.Count;
+            }
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    if (!string.IsNullOrEmpty(((dynamic)dr[i])["color"]))
+                    {
+                        ((dynamic)dr[i])["color"] = Int32.Parse(((dynamic)dr[i])["color"].Substring(2), System.Globalization.NumberStyles.HexNumber);
+                    }
+
+
+                    string text = ((dynamic)dr[i])["text"];
+                    string format = ((dynamic)dr[i])["format"];
+                    if (text.IndexOf(".") != -1 && text.IndexOf(".") == text.LastIndexOf("."))
+                    {
+
+                        if (format.IndexOf("%") != -1)
+                        {
+                            ((dynamic)dr[i])["text"] = (Math.Round(double.Parse(text), 4, MidpointRounding.AwayFromZero) * 100).ToString() + "%";
+
+                        }
+                        else
+                        {
+                            ((dynamic)dr[i])["text"] = Math.Round(double.Parse(text), 2, MidpointRounding.AwayFromZero).ToString();
+                        }
+                    }
+
+
+                    dr[i] = new Dictionary<string, object> { { "text", ((dynamic)dr[i])["text"] }, { "color", ((dynamic)dr[i])["color"] } };
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             string file = @"test.xlsx";
             DataSet sheets;
             sheets = ExcelReader.getAllSheets(file);
+            //regulateData(sheets.Tables[0], 119);
             for(int i = 0; i < sheets.Tables.Count; i ++)
             {
                 DataTable dt = sheets.Tables[i];
@@ -22,6 +70,7 @@ namespace OperateExcel
                     {
                         Console.Write(((dynamic)dt.Rows[j][k])["text"] + " |");
                     }
+                    Console.WriteLine();
                 }
             }
             string outfile = @"newxlsx.xlsx";
